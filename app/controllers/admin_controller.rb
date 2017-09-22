f1fa2259ccd102b4bf6ee64d3e2fa02bdd4dcdff
nauticalnova <nauticalnova@hotmail.com>
2017-09-21 23:42:44 -0600

    added basic users

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,22 @@
+class AdminController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,46 @@
 Rails.application.routes.draw do
+  get 'user/index'
+
+  get 'user/edit'
+
+  get 'user/update'
+
+  get 'user/create'
+
+  get 'user/delete'
+
+  get 'user/show'
+
+  get 'user/new'
+
+  get 'company/index'
+
+  get 'company/edit'
+
+  get 'company/update'
+
+  get 'company/create'
+
+  get 'company/delete'
+
+  get 'company/show'
+
+  get 'company/new'
+
+  get 'admin/index'
+
+  get 'admin/edit'
+
+  get 'admin/update'
+
+  get 'admin/create'
+
+  get 'admin/delete'
+
+  get 'admin/show'
+
+  get 'admin/new'
+
   get 'index/edit'
 
   get 'index/update'
