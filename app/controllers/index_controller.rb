440f28eb49fe49b1e50ce040d0541180c865a978
nauticalnova <nauticalnova@hotmail.com>
2017-09-21 23:24:04 -0600

    added admin

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,19 @@
+class IndexController < ApplicationController
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'index/edit'
+
+  get 'index/update'
+
+  get 'index/create'
+
+  get 'index/delete'
+
+  get 'index/show'
+
+  get 'index/new'
+
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
